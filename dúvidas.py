750aa2fc1d4f63fbbb37be9333575103164062cc
Lucas-far <mizoguchia@hotmail.com>
2021-10-17 16:31:16 -0300

    no need to explain

--- dúvidas.py
+++ dúvidas.py
@@ -2,4 +2,9 @@
 
 """
 Relacionamento de produtos está diretamente relacionado a promoção?
+O que é ICMS?
+O que é PIS?
+O que é COFINS?
+O que é IPI?
+O que CRC?
 """
--- obs.py
+++ obs.py
@@ -1,8 +1,31 @@
 
 
-def tabela_custo():
+def linha_do_tempo():
     """
-    - Uma tabela de custo serve para controle de preço
-    - Uma tabela de custo não deve ser um campo exibível no produto
-    - Cada produto tem um preço de custo, que pode ser atualizado (mensalmente)
+    PROBLEMA 1
+    Nota fiscal referenciada (comercial)     [ Item 171 ]
+        https://vrsystem.info/publico/post.aspx?Id=72c6d24e-5939-4df7-9c43-803f6e815deb
+        https://youtu.be/2gqUX0unjHM
+
+    MOTIVO: Não sei como gerar uma chave de homologação
+
+
+    PROBLEMA 2
+    Emissão de nota em Homologação (outros)     [ Item 532 ]
+        https://vrsystem.info/publico/post.aspx?Id=8f3b1cf5-4dab-4175-9cb5-633ea005cb8d
+
+    MOTIVO: Eu não entendi onde esse tutorial quer chegar
+
+
+    PROBLEMA 3
+    Token captura dados clientes (configurações)     [ Item 411 ]
+        https://vrsystem.info/publico/post.aspx?Id=88ee85c1-abd7-4a15-83e6-b34569e76f9b
+
+    MOTIVO: Não consegui tirar anda desse tutorial
+
+    PROBLEMA 4
+    Pedido de compra (estoque)     [ Item 96 ]
+        https://vrsystem.info/publico/post.aspx?Id=9706e120-927c-40eb-866a-2178920b2c17
+
+    MOTIVO: Ele requer que outro vídeo seja assistido antes (problema 5)
     """
