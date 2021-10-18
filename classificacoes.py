be313cf0eff3822b590e147d14d249231837527d
Lucas-far <mizoguchia@hotmail.com>
2021-10-18 00:18:55 -0300

    no need to explain

---
+++ classificacoes.py
@@ -0,0 +1,40 @@
+
+
+def classificacoes():
+    """
+    Classe    || masculino, feminino
+    Subclasse || roupa_cima P/F/G/GG, roupa_baixo P/F/G/GG, roupa_íntimo P/F/G/GG roupa_mesclada P/F/G/GG
+    Grupo     || estampado
+    Família   ||
+    Marca     ||
+    Impressão ||
+
+    PARA AMBOS
+    Camisa
+    Camiseta
+    Camisa regata
+    Camisa social
+    Moletom
+    Sobretudo
+    Jaqueta
+
+    PARA M & F
+    Short jeans
+    Calção
+    Calça
+    Bermuda
+    Calça Jeans
+
+    PARA F
+    Vestido
+    Saia
+
+    PARA M
+    Cueca
+    Cueca
+    Pijama
+
+    PARA F
+    Calcinha
+    Langerie
+    """
---
+++ curiosidades.py
@@ -0,0 +1,16 @@
+
+
+def curiosidades_():
+    """
+    https://listenx.com.br/blog/como-montar-secoes-de-supermercado/
+    """
+
+
+def destaques():
+    """
+    Cor no plano de contas (financeiro)     [ Item 279 ]
+        https://vrsystem.info/publico/post?Id=5ca39f6d-cb73-414b-8842-aed14de62fcf
+    """
+
+
+print('')
